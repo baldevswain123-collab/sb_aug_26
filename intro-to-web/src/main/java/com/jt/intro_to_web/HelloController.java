2c90347c9746966ad03c201e9795908711b59858
baldevswain123-collab <baldevswain123@gmail.com>
2026-08-26 20:47:00 +0530

    Intro-To-Web

--- intro-to-web/src/main/java/com/jt/intro_to_web/HelloController.java
+++ intro-to-web/src/main/java/com/jt/intro_to_web/HelloController.java
@@ -2,25 +2,71 @@ package com.jt.intro_to_web;
 
 import java.io.PrintWriter;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-  
-@Controller // 1.It marks the class as a Controller class      //which means that class is responsible to handle http request
-public class HelloController {
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-    @RequestMapping("/home") //This annotation hanldes a custom path inside the controller class//path handling
-                            // 
-    public void sayHello(PrintWriter writer){
-        System.out.println("Hello Web");
-        writer.println("<h1>Hello Spring Web</h1> <p>Welcome Home</p>");
-    }
-    @RequestMapping
-    public void landingPage(PrintWriter printWriter){
-        System.out.println("landing Page");
-        printWriter.println("Our First Landing Page");
-    }
-    @RequestMapping("/contact")
-    public String contact (){
-        return"contact-page";
-    }
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+  @RequestMapping("/home")                                                   
+  public void sayHello(PrintWriter writer) {  // PrintWrite is used to write character text directly  
+    System.out.println("Hello Web");        //into the raw HTTP response  stream that gets sent back to clients browser
+    writer.println("<h1>Hello Spring Web</h1> <p>Welcome Home</P>");
+  }
+
+  @RequestMapping("/")
+  public void landingPage(PrintWriter printWriter) {
+    System.out.println("landing Page1");
+    printWriter.println("Our First Landing Page1");
+  }
+
+
+  @RequestMapping("/contact")
+  public String contact() {
+    return "contact-page";
+  }
+
+
+//   @RequestMapping("/submit-details")
+//   public String submitDetails(HttpServletRequest request, Model model) { //When you add Model model as a method parameter, 
+                                                                        // Spring automatically instantiates and manages the object for you.
+//   System.out.println("Submit Details handled");                      //Model (org.springframework.ui.Model) is a data container used to 
+                                                                        // pass data from your Controller (Java code) to your View (HTML/UI template).
+
+//   String name = request.getParameter("name");  
+//   String phone = request.getParameter("phone");
+
+//   // System.out.println("name is:- " + name);
+//   // System.out.println("phone is:- " + phone);
+
+//   model.addAttribute("name1", name);         //It holds key-value pairs that template engines (like Thymeleaf, JSP, 
+//   model.addAttribute("phone", phone);        //// or FreeMarker) read and display on the rendered webpage.
+
+
+//   return "details-page";
+//   }
+// }
+
+  // @RequestMapping(value = "/submit-details", method = RequestMethod.POST )
+  // public String submitDetails(@RequestParam(value = "name1", required = false, defaultValue = "Springboot") String name1, @RequestParam String phone, Model model) {
+
+  //   model.addAttribute("name1", name1);
+  //   model.addAttribute("phone", phone);
+
+  //   return "details-page";
+  // }
+  @RequestMapping(value = "/submit-details", method = RequestMethod.POST )
+  public String submitDetails(@ModelAttribute Person person, Model model) {
+
+    model.addAttribute("name1", person.getname1());
+    model.addAttribute("phone", person.getPhone());
+
+    return "details-page";
+  }
 }
