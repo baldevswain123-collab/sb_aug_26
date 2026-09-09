73dfa37ebe06f7dcc1e5c71f209330d78b270432
baldevswain123-collab <baldevswain123@gmail.com>
2026-09-09 23:18:04 +0530

    UI Of  Expense Tracker

---
+++ expense-tracker-client/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import ExpenseForm from "./components/ExpenseForm";
+import Summary from "./components/Summary";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+
+      <main className="max-w-4xl mx-auto py-4 mt-4">
+        <ExpenseForm />
+        <Summary />
+      </main>
+
+      {/* Footer */}
+    </div>
+  )
+}
